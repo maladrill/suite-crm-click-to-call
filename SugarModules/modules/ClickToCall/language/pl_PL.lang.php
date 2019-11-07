99f2f41bc10e80f4bfa2e67d006347b76045b679
maladrill <maladrill@users.noreply.github.com>
2019-11-07 01:37:39 +0100

    Add files via upload

---
+++ SugarModules/modules/ClickToCall/language/pl_PL.lang.php
@@ -0,0 +1,23 @@
+<?php
+
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
+}
+
+$mod_strings['LBL_CLICKTOCALL_TITLE'] = 'SuiteCRM kliknij żeby zadzwonić (Asterisk)';
+
+$mod_strings['LBL_CLICKTOCALL_ASTERISK_IP']   = 'IP';
+$mod_strings['LBL_CLICKTOCALL_ASTERISK_PORT'] = 'Port';
+
+$mod_strings['LBL_CLICKTOCALL_ASTERISK_USER'] = 'Użytkownik';
+$mod_strings['LBL_CLICKTOCALL_ASTERISK_PASS'] = 'Hasło';
+
+$mod_strings['LBL_CLICKTOCALL_ASTERISK_CHANNELIN']        = 'Kanał wejściowy';
+$mod_strings['LBL_CLICKTOCALL_ASTERISK_CHANNELINCONTEXT'] = 'Kanał wejściowy - kontekst';
+
+$mod_strings['LBL_CLICKTOCALL_ASTERISK_CHANNELOUT']       = 'Kanał wyjściowy';
+$mod_strings['LBL_CLICKTOCALL_ASTERISK_CHANNELOUTCONTEXT'] = 'Kanał wyjściowy - kontekst';
+
+$mod_strings['LBL_CLICKTOCALL_ASTERISK_CALLERID']  = 'Caller ID';
+$mod_strings['LBL_CLICKTOCALL_ASTERISK_VARIABLES'] = 'Zmienne';
+
