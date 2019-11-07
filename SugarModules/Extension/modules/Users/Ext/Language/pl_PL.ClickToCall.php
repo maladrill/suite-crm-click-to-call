980542f5fde00daeee8fe35abae02ff19cf7eff1
maladrill <maladrill@users.noreply.github.com>
2019-11-07 01:36:13 +0100

    Create pl_PL.ClickToCall.php

---
+++ SugarModules/Extension/modules/Users/Ext/Language/pl_PL.ClickToCall.php
@@ -0,0 +1,32 @@
+<?php
+
+if (!isset($mod_strings)) {
+    $mod_strings = array();
+}
+
+$mod_strings['LBL_CLICKTOCALL_PHONE_EXTENSION'] = 'Numer wewnętrzny';
+$mod_strings['LBL_CLICKTOCALL_PANEL'] = 'Asterisk';
+
+
+$mod_strings['LBL_ENTER_ACTIVATION_KEY'] = 'Wprowadź klucz aktywacyjny';
+$mod_strings['LBL_SERIAL_KEY'] = 'Serial ID';
+$mod_strings['LBL_ACTIVATION_KEY'] = 'Klucz aktywacyjny';
+
+$mod_strings['LBL_ADMIN_DEDUP_Message'] = 'Wybierz pola dla uniknięcia duplikatów';
+$mod_strings['LBL_ACCOUNT'] = 'Kontrahenci';
+$mod_strings['LBL_ACCOUNT_NAME'] = 'Nazwa';
+$mod_strings['LBL_ACCOUNT_NUMBER'] = 'Numer telefonu';
+$mod_strings['LBL_ACCOUNT_EMAIL'] = 'Email';
+
+$mod_strings['LBL_CONTACT'] = 'Kontakty';
+$mod_strings['LBL_CONTACT_NAME'] = 'Nazwa';
+$mod_strings['LBL_CONTACT_NUMBER'] = 'Numer telefonu';
+$mod_strings['LBL_CONTACT_EMAIL'] = 'Email';
+
+$mod_strings['LBL_CUSTOMER_INFO'] = 'Zarejestruj się';
+$mod_strings['LBL_CUSTOMER_INFO_NAME'] = 'Nazwa';
+$mod_strings['LBL_CUSTOMER_INFO_COMPANY_NAME'] = 'Nazwa firmy';
+$mod_strings['LBL_CUSTOMER_INFO_NUMBER'] = 'Numer telefonu';
+$mod_strings['LBL_CUSTOMER_INFO_EMAIL'] = 'Email';
+$mod_strings['LBL_REQUEST_KEY'] = 'Poproś o klucz';
+$mod_strings['LBL_UPDATE'] = 'Zaktualizuj';
