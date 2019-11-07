b9ef6adb946bf74c5839747d2ce7459e34c70fe5
maladrill <maladrill@users.noreply.github.com>
2019-11-07 01:36:58 +0100

    Add files via upload

---
+++ SugarModules/Extension/modules/Administration/Ext/Language/pl_PL.ClickToCall.php
@@ -0,0 +1,12 @@
+<?php
+
+if (!isset($mod_strings)) {
+    $mod_strings = array();
+}
+
+$mod_strings['LBL_CLICKTOCALL_TITLE'] = 'SuiteCRM kliknij żeby zadzwonić (Asterisk)';
+$mod_strings['LBL_CLICKTOCALL_DESC'] = '';
+
+$mod_strings['LBL_CLICKTOCALL_CONFIGURATION_TITLE'] = 'Konfiguracja';
+$mod_strings['LBL_CLICKTOCALL_CONFIGURATION_DESC'] = 'Konfiguracja Asteriska';
+
